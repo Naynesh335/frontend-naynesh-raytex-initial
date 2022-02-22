2aa062a7fbe762144c711dfd7982dcbb0ef46c5b
Naynesh335 <nayneshchaudhary@gmail.com>
2022-02-22 12:15:46 +0530

    day four task 1

--- day4/task1/index.js
+++ day4/task1/index.js
@@ -1,5 +1,14 @@
-function form(){
-    var name =document.getElementById("name").value;
-    console.log("name");
-    document.getElementById("name").innerHTML=name;
+function myfunction(){
+    let name =document.getElementById("name").value;
+    let add =document.getElementById("address").value;
+    let pn =document.getElementById("pn").value;
+    let email =document.getElementById("email").value;
+    let cn =document.getElementById("cn").value;
+    let dep =document.getElementById("dep").value;
+    document.getElementById("print").innerHTML=name;
+    document.getElementById("print1").innerHTML=add;
+    document.getElementById("print2").innerHTML=pn;
+    document.getElementById("print3").innerHTML=email;
+    document.getElementById("print4").innerHTML=cn;
+    document.getElementById("print5").innerHTML=dep;
 }
